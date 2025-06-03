e736c617e6b1f3909adaa736991d4f8482ba64d3
sivaprasath <sivaprasath.selvam@coherent.in>
2025-06-03 17:54:56 +0530

    asasa

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('✅ AKS GitHub CI/CD is working!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
