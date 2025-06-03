85746a4b88c1dee528ae96c1fd66052794a58654
sivaprasath <sivaprasath.selvam@coherent.in>
2025-06-03 19:06:16 +0530

    sas

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('✅ AKS GitHub CI/CD is working!');
+app.get("/", (req, res) => {
+  res.send("✅ AKS GitHub CI/CD is working!!");
 });
 
 app.listen(port, () => {
